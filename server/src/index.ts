d484e70c88a6f0a354854f45a2debe860fb6e10f
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-06-10 09:26:24 -0400

    Setup

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(port, () => {
+    console.log(`Server is runing on port $(port)`);
+})
